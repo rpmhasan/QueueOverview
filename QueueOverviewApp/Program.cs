6de8324e055fac30de45407c61b18baeb52e7671
rpmhasan <rpm.hasan@gmail.com>
2015-05-13 16:48:29 +0600

    full

---
+++ QueueOverviewApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueOverviewApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable htable=new Hashtable();
+            htable.Add(1,10);
+            htable[2] = 11;
+            htable[3] = 12;
+
+
+            Console.WriteLine(htable.Count);
+            Console.Read();
+        }
+    }
+}
